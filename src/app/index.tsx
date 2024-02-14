4bf84a6a90ce97a499536263ce5cd1b65d563223
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-02-14 14:35:04 -0300

    feat: category component created and flatlist added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,9 @@
+import { View, FlatList } from "react-native";
+
+import { CategoryButton } from "@/components/category";
 import { Header } from "@/components/header";
-import { View } from "react-native";
+
+import { CATEGORIES } from "@/utils/data/products";
 
 export default function Home() {
   
@@ -8,6 +12,20 @@ export default function Home() {
       <Header
         title="Faça seu pedido"
         cardQuantity={3}
+      />
+
+      <FlatList
+        className="max-h-10 mt-5"
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton 
+            title={item}
+          />
+        )}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        showsHorizontalScrollIndicator={false}
+        horizontal
       />
     </View>
   )
