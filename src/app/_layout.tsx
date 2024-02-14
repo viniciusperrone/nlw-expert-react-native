6ebea713e0286ef4c06ce4e2b11534a025e580b3
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-02-14 13:23:37 -0300

    chore: adjuste config tailwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,32 @@
+import { SafeAreaView } from "react-native";
 import { Slot } from "expo-router";
 
-import "../styles/global.css"
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts
+} from "@expo-google-fonts/inter";
 
-export default Slot
+import "@/styles/global.css";
+import { Loading } from "@/components/loading";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if(!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+  <SafeAreaView className="flex-1 bg-slate-900">
+    <Slot />
+  </SafeAreaView>
+  )
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold"
+      }
+    },
   },
   plugins: [],
 }
