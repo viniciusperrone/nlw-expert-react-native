1b72208a6ed69c3f99f28e48f354bb08d220492b
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-02-17 11:07:39 -0300

    feat: created input component

---
+++ src/components/input.tsx
@@ -0,0 +1,14 @@
+import { TextInput, TextInputProps } from "react-native";
+import colors from "tailwindcss/colors";
+
+export function Input({ ...rest }: TextInputProps) {
+  return(
+    <TextInput
+      className="h-32 bg-slate-800 rounded-md p-4 font-body"
+      placeholderTextColor={colors.slate[400]}
+      textAlignVertical="top"
+      multiline
+      {...rest} 
+    />
+  )
+}
